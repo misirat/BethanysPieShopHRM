011f9860f7a0831375a6c515fb37ec9b234be2af
Md Mohaiminul Islam <mi@netzlab.de>
2023-12-21 00:13:47 +0100

    added and configured Auth0

---
+++ BethanysPieShopHRM.Shared/Model/Marker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Shared.Model
+{
+    public class Marker
+    {
+        public string Description { get; set; } = String.Empty;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public bool? ShowPopup { get; set; }
+    }
+}
--- BethanysPieShopHRM/Pages/EmployeeDetail.razor.cs
+++ BethanysPieShopHRM/Pages/EmployeeDetail.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BethanysPieShopHRM.Models;
 using BethanysPieShopHRM.Services;
 using BethanysPieShopHRM.Shared.Domain;
+using BethanysPieShopHRM.Shared.Model;
 using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.Pages
@@ -14,10 +15,24 @@ namespace BethanysPieShopHRM.Pages
         public string EmployeeId { get; set; }
 
         public Employee? Employee { get; set; } = new Employee();
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
 
         protected async override Task OnInitializedAsync()
         {
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId)); 
+            if (Employee.Latitude.HasValue && Employee.Longitude.HasValue)
+            {
+                MapMarkers = new List<Marker>()
+                {
+                    new Marker
+                    {
+                        Description = $"{Employee.FirstName} {Employee.LastName}",
+                        ShowPopup = false,
+                        X = Employee.Longitude.Value,
+                        Y = Employee.Latitude.Value
+                    }
+                }; 
+            }
         }
     }
 }
