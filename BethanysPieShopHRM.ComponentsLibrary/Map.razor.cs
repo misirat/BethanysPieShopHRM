445ec2411e4e9cc8a1d4b0c4676bfbf61cdace2d
Md Mohaiminul Islam <mi@netzlab.de>
2023-12-21 00:13:15 +0100

    added map as a razor class library

---
+++ BethanysPieShopHRM.ComponentsLibrary/Map.razor.cs
@@ -0,0 +1,26 @@
+﻿using BethanysPieShopHRM.Shared.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BethanysPieShopHRM.ComponentsLibrary
+{
+    public partial class Map
+    {
+        string elementId = $"map-{Guid.NewGuid():D}";
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Parameter]
+        public double Zoom { get; set; }
+        [Parameter]
+        public List<Marker> Markers { get; set; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync(
+                "deliveryMap.showOrUpdate",
+                elementId,
+                Markers);
+        }
+    }
+}
